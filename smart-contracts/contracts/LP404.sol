8b68c1033490343504a6ddeab018ea3b5cfc275c
rickylago2 <92766571+datboi-1337@users.noreply.github.com>
2024-07-03 14:09:39 -0400

    Adjustments
    -Adjusted ERC404 import to use local
    -Updated NatSpec to add descriptions
    -Organized into sections for readability
    -Adjusted some variable/argument names to be more in-line with solidity naming conventions
    -Added dna to Attributes mapping. Since we're already calculating off-chain, we can simply write the value rather than trying to calculate on chain.
    -Adjusted setAttributes to include _dna as argument. Removed requirement for 'attributes already set', as tokenId will be re-used if burnt.
    -Adjusted setAttributes to create a new object at the index rather than attempting to push.
    -Added tokenUri. Currently set to use contract and tokenId as keys for nft viewer, but that's subject to change

--- smart-contracts/contracts/LP404.sol
+++ smart-contracts/contracts/LP404.sol
@@ -1,114 +1,122 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
-import "https://github:Pandora-Labs-Org/erc404/blob/main/contracts/examples/ERC404Example.sol";
+import "erc404/contracts/ERC404.sol";
 
 contract LP404 is Ownable, ERC404 {
-    /**
-     * @dev Metadata structure for the ERC721s
-     */
-    struct attributes {
-        string[] value;
-        string[] trait_types;
+    struct Attributes {
+        string[] traitTypes;
+        string[] values;
+        bytes32 dna;
     }
 
-    mapping(uint => attributes[]) private _attributes; // Attributes of the ERC721s
-    mapping(bytes64 => bool) private _uniqueness; // Keeps track of the uniqueness of the attributes
+    mapping(uint => Attributes) private attributes; // tokenId => Attributes of the ERC721s
+    mapping(bytes32 => bool) private uniqueness; // dna => bool. Keeps track of the uniqueness of the attributes
+    mapping(uint => bool) private circulating; // tokenId => bool. Keeps track of the circulating status of the ERC721s
+
+    string internal description = "I am a description";
+    string internal uri = "nft-viewer.com/";
 
     /**
-     *
-     * @param name_
-     * @param symbol_
-     * @param decimals_
-     * @param maxTotalSupplyERC721_
-     * @param initialOwner_
-     * @param initialMintRecipient_
+     * @param _name Token name
+     * @param _symbol Token Symbol
+     * @param _decimals Decimals
+     * @param _maxTotalSupplyERC721 Max suppy for NFT
+     * @param _initialOwner Owner
+     * @param _initialMintRecipient Transfer Exemption
      */
     constructor(
-        string memory name_,
-        string memory symbol_,
-        uint8 decimals_,
-        uint256 maxTotalSupplyERC721_,
-        address initialOwner_,
-        address initialMintRecipient_
-    ) ERC404(name_, symbol_, decimals_) Ownable(initialOwner_) {
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _maxTotalSupplyERC721,
+        address _initialOwner,
+        address _initialMintRecipient
+    ) ERC404(_name, _symbol, _decimals) Ownable(_initialOwner) {
         // Do not mint the ERC721s to the initial owner, as it's a waste of gas.
-        _setERC721TransferExempt(initialMintRecipient_, true);
-        _mintERC20(initialMintRecipient_, maxTotalSupplyERC721_ * units);
+        _setERC721TransferExempt(_initialMintRecipient, true);
+        _mintERC20(_initialMintRecipient, _maxTotalSupplyERC721 * units);
     }
 
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~ Modifiers ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~ Mint Functions ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~ Setters ~~~~~~~~~~~~~~~~~~~~~~~~~
+    /**
+     * @dev Set the attributes and uniqueness of an ERC721
+     * @notice Setting this to onlyOwner for now, but can be changed to a different modifier
+     * @param _tokenId the id of the ERC721
+     * @param _values the attributes
+     * @param _traitTypes the trait types of the attributes
+     */
+    function setAttributes(uint _tokenId, string[] memory _values, string[] memory _traitTypes, bytes32 _dna) 
+    internal onlyOwner {
+        // Validate array lengths and attribute uniqueness
+        require(_values.length == _traitTypes.length, "Value and traitTypes length mismatch");
+        require(!uniqueness[_dna], "Attributes are not unique");
+        
+        // Set the attributes
+        Attributes memory newAttr = Attributes(
+            _traitTypes,
+            _values,
+            _dna
+        );
+
+        attributes[_tokenId] = newAttr;
+        uniqueness[_dna] = true;
+    }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~ Getters ~~~~~~~~~~~~~~~~~~~~~~~~~
     /**
      * @dev Gets the attributes of an ERC721
-     * @param tokenId_
+     * @param _tokenId the id of the ERC721
      */
-    function getAttributes(
-        uint tokenId_
-    ) external view returns (attributes[] memory) {
-        return _attributes[tokenId_];
+    function getAttributes(uint _tokenId) external view returns (string[] memory) {
+        return attributes[_tokenId].values;
     }
 
     /**
      * @dev Checks if the attributes are unique
-     * @param dna_ the hash of the attributes
+     * @param _dna the hash of the attributes
      */
-    function checkUniqueness(bytes64 dna_) external view returns (bool) {
-        return _uniqueness[dna_];
+    function checkUniqueness(bytes32 _dna) external view returns (bool) {
+        return uniqueness[_dna];
     }
 
-    /**
-     * @dev Set the attributes and uniqueness of an ERC721
-     * @notice Setting this to onlyOwner for now, but can be changed to a different modifier
-     * @param tokenId_
-     * @param value_
-     * @param trait_types_
-     */
-    function setAttributes(
-        uint tokenId_,
-        string[] memory value_,
-        string[] memory trait_types_
-    ) internal onlyOwner {
-        // Check if the attributes are already set
-        require(_attributes[tokenId_].length == 0, "Attributes already set");
-        require(
-            value_.length == trait_types_.length,
-            "Value and trait_types length mismatch"
-        );
-        // Set the attributes
-        _attributes[tokenId_].push(
-            attributes({value: value_, trait_types: trait_types_})
-        );
-        // Hash the attributes by encoding them in base64 then hashing with keccak256
-        // Logic: data:application/json;base64, + base64(attributes)
-        // I borrowed the logic from the snippet Richard posted in the Discord
-        bytes64 dna = keccak256(
-            abi.encodePacked(
-                "data:application/json;base64,",
-                Base64.encode(
-                    bytes(
-                        abi.encodePacked(
-                            '{"value": "',
-                            value_,
-                            '", "trait_types": "',
-                            trait_types_,
-                            '"}'
-                        )
-                    )
-                )
-            )
-        );
-        // Check if the attributes are unique and set them
-        require(!_uniqueness[dna], "Attributes are not unique");
-        _uniqueness[dna] = true;
+    function tokenURI(uint256 _id) public view override returns (string memory) {
+        require(circulating[_id], "NFT is not in circulation");
+        string memory tokenName = string(abi.encodePacked('[LPNFT] ', name, ' #', Strings.toString(_id)));
+        string memory imageLink = string(abi.encodePacked(
+            uri, 
+            Strings.toHexString(uint256(uint160(address(this))), 20), 
+            '/', 
+            Strings.toString(_id)
+        ));
+        
+        string memory attrStr = '[';
+
+        for (uint i = 0; i < attributes[_id].values.length; i++) {
+            attrStr = string.concat(attrStr, string(abi.encodePacked('{"',attributes[_id].traitTypes[i], '": "', attributes[_id].values[i], '"}')));
+            i == attributes[_id].values.length - 1 ? attrStr = string.concat(attrStr, ']') : attrStr = string.concat(attrStr, ',');
+        }
+
+        return string(abi.encodePacked(
+      'data:application/json;base64,', Base64.encode(bytes(abi.encodePacked(
+        '{',
+        '"name": "', tokenName, '", ',
+        '"description": "', description, '", ',
+        '"image": "', imageLink, '", ',
+        '"attributes": ', attrStr,
+        '}')))
+      ));
     }
 
-    // This came with the example contract, not sure if it's needed
-    function setERC721TransferExempt(
-        address account_,
-        bool value_
-    ) external onlyOwner {
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~ Admin Functions ~~~~~~~~~~~~~~~~~~~~~~~~~
+    function setERC721TransferExempt(address account_, bool value_) external onlyOwner {
         _setERC721TransferExempt(account_, value_);
     }
 }
