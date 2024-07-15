f361e4289818ddcc5a90201033130f8e9784b28f
rickylago2 <rickylago2@gmail.com>
2024-07-14 22:27:12 -0400

    Update hardhat.config.js
    Added optimization.

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -19,9 +19,33 @@ module.exports = {
   },
   solidity: {
     compilers: [
-      { version: "0.6.5" },
-      { version: "0.8.20" },
-      { version: "0.8.24" }
+      { 
+        version: "0.6.5",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      { 
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      { 
+        version: "0.8.24",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
     ]
   }
 };
